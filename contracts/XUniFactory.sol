cf0a8cf6fe2745cc2f7723d1468ef8723ac368a1
masaun <masanoriuno@gmail.com>
2021-03-15 15:39:59 +0900

    Define stake() / unStake() in the XUniFactory.sol

--- contracts/XUniFactory.sol
+++ contracts/XUniFactory.sol
@@ -17,11 +17,39 @@ contract XUniFactory is ERC20 {
         uni = _uni;
     }
 
-    // Enter the bar. Pay some UNIs. Earn some shares.
+    // Stake the UNIs into this contract. Earn some shares.
     // Locks UNI and mints xUNI
-    function enter(uint256 _amount) public {}
+    function stake(uint256 _amount) public {
+        // Gets the amount of UNI locked in the contract
+        uint256 totalUni = uni.balanceOf(address(this));
 
-    // Leave the bar. Claim back your UNIs.
+        // Gets the amount of xUNI in existence
+        uint256 totalShares = totalSupply();
+        
+        // If no xUNI exists, mint it 1:1 to the amount put in
+        if (totalShares == 0 || totalUni == 0) {
+            _mint(msg.sender, _amount);
+        } 
+        // Calculate and mint the amount of xUNI the UNI is worth. The ratio will change overtime, as xUNI is burned/minted and UNI deposited + gained from fees / withdrawn.
+        else {
+            uint256 mintAmount = _amount.mul(totalShares).div(totalUni);
+            _mint(msg.sender, mintAmount);
+        }
+        
+        // Lock the UNI in the contract
+        uni.transferFrom(msg.sender, address(this), _amount);
+
+    }
+
+    // Unstake the UNIs and burn xUNIs. Claim back your UNIs.
     // Unlocks the staked + gained UNI and burns xUNI
-    function leave(uint256 _share) public {}
+    function unStake(uint256 _share) public {
+        // Gets the amount of xUNI in existence
+        uint256 totalShares = totalSupply();
+
+        // Calculates the amount of UNI the xUNI is worth
+        uint256 redeemAmount = _share.mul(uni.balanceOf(address(this))).div(totalShares);
+        _burn(msg.sender, _share);
+        uni.transfer(msg.sender, redeemAmount);
+    }
 }
