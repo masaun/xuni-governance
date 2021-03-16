733103bb527e07e2ebe279f343c9410778a5b0fe
masaun <masanoriuno@gmail.com>
2021-03-16 21:57:26 +0900

    [Test]: Mint UNI token to user1

--- test/test-local/XUniFactory.test.js
+++ test/test-local/XUniFactory.test.js
@@ -17,8 +17,10 @@ const UniToken = artifacts.require("Uni")
 contract("XUniFactory", function(accounts) {
     /// Acccounts
     let deployer = accounts[0]
-    let admin = accounts[1]
+    let minter = accounts[1]
     let user1 = accounts[2]
+    let user2 = accounts[3]
+    let user3 = accounts[4]
 
     /// Global contract instance
     let uniToken
@@ -30,12 +32,15 @@ contract("XUniFactory", function(accounts) {
 
     describe("Setup smart-contracts", () => {
         it("Deploy the UniToken contract instance", async () => {
+            const now = await time.latest()
+
             // @param account The initial account to grant all the tokens
             // @param minter_ The account with minting ability
             // @param mintingAllowedAfter_ The timestamp after which minting may occur
             const account = deployer
-            const minter_ = admin
-            const mintingAllowedAfter_ = await time.latest() + 1  /// [Note]: Get the latest timestamp
+            const minter_ = minter
+            const mintingAllowedAfter_ = Number(now) + 3600  /// [Note]: The latest timestamp + 1 hour (3600 seconds)
+            console.log('=== mintingAllowedAfter_ ===', mintingAllowedAfter_)
             
             uniToken = await UniToken.new(account, minter_, mintingAllowedAfter_, { from: deployer })
             UNI_TOKEN = uniToken.address
@@ -49,6 +54,24 @@ contract("XUniFactory", function(accounts) {
         it("[Log]: Deployed-contracts addresses", async () => {
             console.log("\n=== UNI_TOKEN ===", UNI_TOKEN)
             console.log("\n=== XUNI_FACTORY (xUNI) ===", XUNI_FACTORY)
+        })
+    })
+
+    describe("Preparation", () => {
+        it("Mint 100 UNI token to 3 users", async () => {
+            const mintingAllowedAfter = await uniToken.mintingAllowedAfter()
+            console.log('=== mintingAllowedAfter ===', String(mintingAllowedAfter))
+
+            const mintAmount = web3.utils.toWei('100', 'ether')
+
+            await time.increaseTo(Number(mintingAllowedAfter) + 600)         
+            txReceipt1 = await uniToken.mint(user1, mintAmount, { from: minter })
+
+            // await time.increaseTo(Number(mintingAllowedAfter) + 1200)     
+            // txReceipt2 = await uniToken.mint(user2, mintAmount, { from: minter })
+
+            // await time.increaseTo(Number(mintingAllowedAfter) + 1800)
+            // txReceipt3 = await uniToken.mint(user3, mintAmount, { from: minter })
         })
     })
 
